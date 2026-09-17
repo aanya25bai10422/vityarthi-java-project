24ff82853f4228da1434f68f546ebef4222991e5
aanya25bai10422 <aanya.25bai10422@vitbhopal.ac.in>
2026-09-17 19:57:49 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,64 @@
+import java.io.Serializable;
+
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String title;
+    private String author;
+    private String category;
+
+    private int totalCopies;
+    private int availableCopies;
+
+    public Book(int id, String title, String author, String category, int totalCopies) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.totalCopies = totalCopies;
+        this.availableCopies = totalCopies;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getTotalCopies() {
+        return totalCopies;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public int getIssuedCopies() {
+        return totalCopies - availableCopies;
+    }
+
+    public void issueCopy() {
+        if (availableCopies > 0) {
+            availableCopies--;
+        }
+    }
+
+    public void returnCopy() {
+        if (availableCopies < totalCopies) {
+            availableCopies++;
+        }
+    }
+}
 
